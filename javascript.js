27590e3a9afea330681880e7b6bdf78f98227273
you-know1993 <35299180+you-know1993@users.noreply.github.com>
2018-06-24 23:27:31 +0200

    Responsive youtube fix

--- javascript.js
+++ javascript.js
@@ -120,3 +120,34 @@ $(window).bind('scroll', function () {
 });
 
 //Inspired by http://jsfiddle.net/j6odpxsk/
+
+$(function() {
+
+    var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed"),
+    $fluidEl = $("figure");
+
+  $allVideos.each(function() {
+
+    $(this)
+      // jQuery .data does not work on object/embed elements
+      .attr('data-aspectRatio', this.height / this.width)
+      .removeAttr('height')
+      .removeAttr('width');
+
+  });
+
+  $(window).resize(function() {
+
+    var newWidth = $fluidEl.width();
+    $allVideos.each(function() {
+
+      var $el = $(this);
+      $el
+          .width(newWidth)
+          .height(newWidth * $el.attr('data-aspectRatio'));
+
+    });
+
+  }).resize();
+
+});
